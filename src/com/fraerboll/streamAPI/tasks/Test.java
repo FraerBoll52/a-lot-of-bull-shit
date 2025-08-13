4617910693a23780ea7d3afea41bbbe389dbe94e
FraerBoll52 <kzeltov110@gmail.com>
2025-08-13 21:30:28 +0400

    deleted useful comments and some old code, also refactored files in correct directory

--- src/com/fraerboll/streamAPI/tasks/Test.java
+++ src/com/fraerboll/streamAPI/tasks/Test.java
@@ -1,8 +1,6 @@
-package com.fraerboll.tasks;
+package com.fraerboll.streamAPI.tasks;
 
 import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;

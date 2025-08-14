5eb614f4b5fe74725457d93eeef58b9fe45b8743
FraerBoll52 <kzeltov110@gmail.com>
2025-08-14 18:02:54 +0400

    Solved task6

--- src/com/fraerboll/streamAPI/tasks/unsolved/Task6.java
+++ src/com/fraerboll/streamAPI/tasks/unsolved/Task6.java
@@ -1,7 +1,24 @@
 package com.fraerboll.streamAPI.tasks.unsolved;
 
+import com.fraerboll.streamAPI.tasks.records.Product;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class Task6 {
     public static void main(String[] args) {
+        List<Product> products = Arrays.asList(
+                new Product("Laptop", "Electronics", 999.99),
+                new Product("Phone", "Electronics", 699.99),  // Опечатка в "Electronics"!
+                new Product("Book", "Education", 19.99),
+                new Product("Monitor", "Electronics", 199.99)
+        );
+
+        Double sum  = products.stream()
+                .filter(p->p.category().toLowerCase().matches("electronic|electro") || p.category().toLowerCase().contains("elect"))
+                .mapToDouble(Product::price)
+                .sum();
+        System.out.println(sum);
 
     }
 }
